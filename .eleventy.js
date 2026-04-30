c7334bfe3e51c02503fcc8b070b46022ee155694
Ben <meowcat@meowcat.site>
2026-04-30 17:04:26 +0100

    set it to output

--- .eleventy.js
+++ .eleventy.js
@@ -60,7 +60,7 @@ module.exports = function (eleventyConfig) {
       input: "source",
       includes: "_layouts",
       data: "_data",
-      output: "./deploy/_site",
+      output: "_site",
     },
   };
 };
